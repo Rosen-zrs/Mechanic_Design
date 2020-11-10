84297173c9f837d3faf04325123e69e53035cac5
Rosen-zrs <1018477962@qq.com>
2020-11-11 01:00:51 +0800

    机械臂遥控框架

--- USER/main.c
+++ USER/main.c
@@ -2,12 +2,12 @@
 #include "math.h"
 #include "stdlib.h"
 
-
-
+#define OUTPUT
 
  int main(void)
  {	
 	 u8 key; 
+	 int flag = 0;
 	 s16 speed,speed1,speed2; 
 	 s16 swerve;           //转弯量	 
  
@@ -34,8 +34,6 @@
 		 if( !PS2_RedLight())
 		 {
 			 delay_ms(50);	 //延时很重要不可去
-			 
-			 LED0 = ~LED0;
 				
 			 key = PS2_DataKey();	 //手柄按键捕获处理
 				
@@ -73,28 +71,74 @@
 			 }
 			 Set_qian_motor(speed1, speed2);
 			 Set_hou_motor(speed1, speed2);
-		 }
-		 if(key!=0)                   //有按键按下
-     {
-			 LED1 = ~LED1;
-		   printf("  \r\n   %d  is  light  \r\n",Data[1]);//ID
-			 printf("  \r\n   %d  is  pressed  \r\n",key);
-			 if(key == 11)
+			 
+			 if(key != 0)
 			 {
-				PS2_Vibration(0xFF,0x00);  //发出震动后必须有延时  delay_ms(1000);
-				delay_ms(500);
+				 switch(key)
+				 {
+					 //机械臂一级关节
+					 case PSB_GREEN:
+					 {
+						 LED0 = ~LED0;
+						 /**********************************************/
+						 
+						 break;
+					 }
+					 //机械臂二级关节
+					 case PSB_BLUE:
+					 {
+						 LED1 = ~LED1;
+						 /**********************************************/
+						 
+						 break;
+					 }
+					 //抓取
+					 case PSB_RED:
+					 {
+						 if(flag)
+						 {
+							 /*******************放开方块*******************/
+							 
+							 
+							 /**********************************************/
+							 flag = 0;
+						 }
+						 else
+						 {
+							 /*******************抓取方块*******************/
+							 
+							 
+							 /**********************************************/
+							 
+							 flag = 1;
+						 }
+						 break;
+					 }
+					 case PSB_L1:
+					 {
+						 PS2_Vibration(0xFF,0x00);  //发出震动后必须有延时  delay_ms(1000);
+						 delay_ms(500);
+						 PS2_Vibration(0x00,0x00); 
+						 break;
+					 }
+					 case PSB_R1:
+					 {
+						 PS2_Vibration(0x00,0xFF);  //发出震动后必须有延时  delay_ms(1000);
+						 delay_ms(500);
+						 PS2_Vibration(0x00,0x00); 
+						 break;
+					 }
+				 }
+				 
+				 #ifdef OUTPUT
+				 printf("  \r\n   %d  is  pressed  \r\n",key);
+				 #endif
 			 }
-				else if(key == 12)
-				{
-					PS2_Vibration(0x00,0xFF);  //发出震动后必须有延时  delay_ms(1000);
-					delay_ms(500);
-				}
-				else
-					PS2_Vibration(0x00,0x00); 
-			}
-		 printf("%5d %5d \r\n", speed1, speed2);
-//			printf(" %5d %5d %5d %5d\r\n",PS2_AnologData(PSS_LX),PS2_AnologData(PSS_LY),
-//																		PS2_AnologData(PSS_RX),PS2_AnologData(PSS_RY) );
+		 }
+		 #ifdef OUTPUT
+		 #else
+		 printf("speed : %5d %5d \r\n", speed1, speed2);
+		 #endif
 		}
 }
 
